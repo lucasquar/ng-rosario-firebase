dca3c2c594fadc592bd236252a819db259a56854
Lucas Quartara <lucas.quartara@gmail.com>
2022-06-11 15:55:33 -0300

    Sorteo fisrt commit

---
+++ ng-rosario-sorteo/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'ng-rosario-sorteo',
+    appId: '1:607781561658:web:0816d2984f5319c1139c03',
+    storageBucket: 'ng-rosario-sorteo.appspot.com',
+    apiKey: 'AIzaSyBk6CdkCgPfSVLhljJuk6AZn_1fWodVu3g',
+    authDomain: 'ng-rosario-sorteo.firebaseapp.com',
+    messagingSenderId: '607781561658',
+  },
+  production: true
+};
---
+++ ng-rosario-sorteo/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'ng-rosario-sorteo',
+    appId: '1:607781561658:web:0816d2984f5319c1139c03',
+    storageBucket: 'ng-rosario-sorteo.appspot.com',
+    apiKey: 'AIzaSyBk6CdkCgPfSVLhljJuk6AZn_1fWodVu3g',
+    authDomain: 'ng-rosario-sorteo.firebaseapp.com',
+    messagingSenderId: '607781561658',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.

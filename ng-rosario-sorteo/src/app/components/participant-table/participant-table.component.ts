184bc28028d30de205d15c30b2b49284e055428f
Pablo Mancinelli <mpablo182@gmail.com>
2022-06-23 11:53:19 -0300

    WIP Apply Angular Animations

--- ng-rosario-sorteo/src/app/components/participant-table/participant-table.component.ts
+++ ng-rosario-sorteo/src/app/components/participant-table/participant-table.component.ts
@@ -1,10 +1,23 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { IParticipant } from 'src/app/models/participant.interface';
 
 @Component({
   selector: 'app-participant-table',
   templateUrl: './participant-table.component.html',
-  styleUrls: ['./participant-table.component.css']
+  styleUrls: ['./participant-table.component.css'],
+  animations: [
+    trigger('fade', [      
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(1000, style({opacity: 1}))
+      ]),
+      transition('* => void', [
+        animate(1000, style({opacity: 0}))
+      ])
+    ])
+
+]
 })
 export class ParticipantTableComponent implements OnInit {
 
